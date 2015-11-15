276b9f3a3218bfa670f3222058a26bd20d89ee35
James Ross <jamr2@pdx.edu>
2015-11-15 19:57:48 +0000

    made a pull from input buffer macro, resolved a redundant if statment with sampNum, removing a stupid conditional from my inner for loop

--- sedih/mushMonster/main.c
+++ sedih/mushMonster/main.c
@@ -1,20 +1,18 @@
-#define __SED_ERR__
-#define __SED_NUM__
-#define __SED_LINUX__
-#include "../sedhead/sedhead.h"
-
-/* executes method one and returns the results in an int corresponding
-   to the result that will be printed. returns the samples min */
-int method_one(int prev, int cur);
-/* goes through the file, pulls all the stuff, calls the methods for the stuff.
-   that stuff.
-   results[0] = method1;
-   results[1] = method2; */
-int gather_results(int *results[]);
-/* print the results of each method */
-void display_results(int *methOne, int numTests);
+#include "mush.h"
 
 #define IN_BUF_ 1000000
+#define TM_BUF_ 10 /* an integer is only up to 10 digits, I.E 10 characters. */
+
+/* buffer to get information from (char*). Where to place the result (char*),
+   Current index inside of buffer. ... are the stopping conditionals */
+#define getBufLine(buffer, res, bfPl, ...)                      \
+{                                                               \
+    int _TM_;                                                   \
+    for(_TM_ = 0; __VA_ARGS__; ++tm, ++bfPl){                   \
+        res[_TM_] = buffer[bfPl];}                              \
+    ++bfPl; /* skip newline */                                  \
+    res[_TM_] = '\0';                                           \
+}
 
 int main()
 {
@@ -26,40 +24,33 @@ int main()
 
 int gather_results(int *result[])
 {
-    int minOne = 0;   /* minimum method 1 */
-    int numTests = 0; /* number of tests in file */
-    int sampNum  = 0; /* ammount of samples */
-    int prevVal  = 0; /* previous sample values */
-    int curVal   = 0; /* current sample value */
-    int bfPl     = 0; /* place from inBuff */
-    int tm       = 0; /* place in temp */
+    int minOne   = 0;       /* minimum method 1 */
+    int numTests = 0;       /* number of tests in file */
+    int sampNum  = 0;       /* ammount of samples */
+    int prevVal  = 0;       /* previous sample values */
+    int curVal   = 0;       /* current sample value */
+    int bfPl     = 0;       /* place from inBuff */
+    int tm       = 0;       /* place in numStr */
     int i        = 0;
     int j        = 0;
-    int fd       = 0; /* file descriptior */
-    ssize_t retBytes = 0; /* byte value returned from a read or write */
-    char inBuff[IN_BUF_] = {'\0'};
-    char temp[IN_BUF_ + 1] = {'\0'};
+    int fd       = 0;       /* file descriptior */
+    ssize_t retBytes = 0;   /* byte value returned from a read or write */
+    char inBuff[IN_BUF_] = {'\0'};  /* input buffer ;) */
+    char numStr[TM_BUF_] = {'\0'};  /* String to be converted to an int from buf */
     
-    /* open the file */
     fd = open("smallpractice", O_RDWR);
     if(fd == -1){
         errExit("gather_results: Opening file failed");}
-
-    /* do the stuff with the file i/o ;) */
-    retBytes = read(fd, (void*)inBuff, IN_BUF_);
-    /* potential newline at end of file */
-    if(retBytes != IN_BUF_){
-        inBuff[retBytes-1] = '\0';}
     
-    /* get input of first line. */
-    for(tm = 0/*bfPl*/; inBuff[bfPl] != '\n'; ++tm, ++bfPl){
-        temp[tm] = inBuff[bfPl];}
-    ++bfPl; /* skip newline */
-    temp[tm] = '\0';
-    /* aquire the number represented */
-    numTests = getInt(temp, 0, "numTests");
+    /* fill the input buffer */
+    retBytes = read(fd, (void*)inBuff, IN_BUF_);
+    if(retBytes != IN_BUF_){ /* Potential newline at end of file */
+        inBuff[retBytes-1] = '\0';}
+   
+    /* get number of tests */
+    getBufLine(inBuff, numStr, bfPl, inBuff[bfPl] != '\n');
+    numTests = getInt(numStr, 0, "numTests");
 
-    /* allocate room for results */
     result[0] = (int*) malloc(sizeof(int)*numTests);
     result[1] = (int*) malloc(sizeof(int)*numTests);
 
@@ -68,46 +59,37 @@ int gather_results(int *result[])
     {
         minOne = 0;
         /* get input from #of samples */
-        for(tm = 0 /*bfPl*/; inBuff[bfPl] != '\n'; ++tm, ++bfPl){
-            temp[tm] = inBuff[bfPl];}
-        ++bfPl;  /* skip newline */
-        temp[tm] = '\0';
-        /* aquire the number represented */
-        sampNum = getInt(temp, 0, "sampNum");
+        getBufLine(inBuff, numStr, bfPl, inBuff[bfPl] != '\n');
+        sampNum = getInt(numStr, 0, "sampNum");
 
         /* pull first sample from file */
-        for(tm = 0; inBuff[bfPl] != ' ' && inBuff[bfPl] != '\n'; ++tm, ++bfPl){
-            temp[tm] = inBuff[bfPl];}
-        ++bfPl; /* skip newline or space */
-        temp[tm] = '\0';
-        prevVal = getInt(temp, 0, "prevVal");
+        getBufLine(inBuff, numStr, bfPl, inBuff[bfPl] != '\n' && inBuff[bfPl] != ' ');
+        prevVal = getInt(numStr, 0, "prevVal");
 
-        if(sampNum == 1){
-            minOne = prevVal;
-            /*results[1][i] = prevVal;*/}
-
-        for(j = 0; j < sampNum-1 && sampNum != 1; ++j)
+        if(sampNum == 1)
         {
-            for(tm = 0; inBuff[bfPl] != ' ' && inBuff[bfPl] != '\n'; ++tm, ++bfPl){
-                temp[tm] = inBuff[bfPl];}
-            ++bfPl; /* skip newline or space */
-            temp[tm] = '\0';
-            curVal = getInt(temp, 0, "curVal");
+            minOne = prevVal;
+            /*results[1][i] = prevVal;*/
+        }
+        
+        --sampNum; /* prevVal holds the first sample */
+        for(j = 0; j < sampNum; ++j)
+        {
+            getBufLine(inBuff, numStr, bfPl, inBuff[bfPl] != '\n' && inBuff[bfPl] != ' ');
+            curVal = getInt(numStr, 0, "curVal");
             
-            /* get the minimums */
+            /* get the mins */
             minOne += method_one(prevVal, curVal);
             /* minTwo += method_two() */
 
-            /* set next previous */
             prevVal = curVal;
-        } /* end for */
+        }
 
         /* place result in correct place */
         result[0][i] = minOne;
         /* result[1][i] = minTwo; */
     } 
     
-    /* close the file */
     if(close(fd) == -1){
         errExit("gather_results: close() failure");}
 

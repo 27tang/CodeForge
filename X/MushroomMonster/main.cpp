ca5059d6bc2d7450eeecf0c38942ea8ef3ddebf5
Xuan <ytang@pdx.edu>
2015-11-14 19:26:48 -0800

    ...

---
+++ X/MushroomMonster/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int numOfCases = 0;
+  cin >> numOfCases; cin.ignore();
+  
+  for(int i = 0; i < numOfCases; ++i)
+  {
+    int numOfIntervals = 0;
+    cin >> numOfIntervals; cin.ignore();
+  }
+  
+  
+
+  return 0;
+}

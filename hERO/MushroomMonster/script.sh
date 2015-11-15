0e3654bdf8970c5d1e141d2e2200038e068ce7f9
Rohan Elukurthy <rohane@cat.pdx.edu>
2015-11-14 21:59:36 -0800

    Mushroom monster almost done

--- hERO/MushroomMonster/script.sh
+++ hERO/MushroomMonster/script.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-cat testfile | \
+cat A-small-practice.in | \
 	awk '
 		BEGIN {
+			count = 1;
 			line = 1;
-			MushEaten = 0;
-			MushOnPlate = 0;
+			MushEaten_1 = 0;
+			MushOnPlate_1 = 0;
+			MushOnPlate_2 = 0;
+			MushEaten_1 = 0;
+			mostRate = 0;
 		}
 		{
 			if(line % 2 != 0 && line != 1)
 			{
-				MushOnPlate = $1;
+				MushOnPlate_1 = $1;
 				for(i = 2; i <= NF ; ++i)
 				{
-					if($i < MushOnPlate){
-						MushEaten += (MushOnPlate - $i);
+					if($i < MushOnPlate_1){
+						MushEaten_1 += (MushOnPlate_1 - $i);
 					}
-					MushOnPlate = $i;
+					MushOnPlate_1 = $i;
 				}
-				printf "%d\n",MushEaten;
-				MushEaten = 0;MushOnPlate = 0;
+
+				for(i = 1; i < NF ; ++i)
+				{
+					if($i > $(i+1)){
+						rate = ($i - $(i+1));
+						if ( rate > mostRate )
+							mostRate = rate;
+					}
+				}
+
+				for(i = 1; i < NF ; ++i)
+				{
+					if($i <= mostRate)
+					{
+						MushEaten_2 += $i;
+					}
+					else
+						MushEaten_2 += mostRate;
+				}
+				printf "Case #%d: %d %d\n",count,MushEaten_1,MushEaten_2;
+				MushEaten_1 = 0;MushOnPlate_1 = 0;mostRate=0;MushEaten_2 = 0;
+				++count;
 			}
 			++line;
 		}

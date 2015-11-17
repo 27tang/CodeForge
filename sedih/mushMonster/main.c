23b3cb763856dfc23ac1528741bae4b6b833885c
James Ross <jamr2@pdx.edu>
2015-11-17 00:21:08 +0000

    Complete, i think its right.... Runs in 0.015s w/o register keywords

--- sedih/mushMonster/main.c
+++ sedih/mushMonster/main.c
@@ -1,13 +1,14 @@
 #include "mush.h"
 
-#define IN_BUF_ 10000
+#define IN_BUF_ 512
 #define TM_BUF_ 16
 
 /* allocate an input buffer */
 #define alloc_buff(buff)                                                       \
 {                                                                              \
     ssize_t _retBytes = 0;                                                     \
-    _retBytes = read(fd, (void*) buff, IN_BUF_-1);                             \
+    if((_retBytes = read(fd, (void*) buff, IN_BUF_-1)) == -1){                         \
+        errExit("alloc_buf(): read() failure");}                               \
     (_retBytes < IN_BUF_-1) ? (buff[_retBytes] = '\0')                         \
                             : (buff[IN_BUF_-1] = '\0');                        \
 } /* end alloc_buff */
@@ -27,22 +28,26 @@
     {                                                                          \
         resStr[_TM_] = *bfPl;                                                  \
         ++bfPl;                  /* increase buff placement */                 \
-        if(*bfPl == '\0'){                                                     \
+        if(*bfPl == '\0'){ /* reached end of current buffer */                 \
             setBuf(inBuf, bfPl);}                                              \
     } /* end for */                                                            \
     ++bfPl;                                                                    \
     resStr[_TM_] = '\0';                                                       \
+    if(*bfPl == '\0'){ /* reached end of current buffer */                     \
+        setBuf(inBuf, bfPl);}                                                  \
 } /* end alloc_bff */
 
 int main()
 {
     int *result[2] = {NULL};
-    gather_results(result);
+    int numTests = 0;
+    numTests = gather_results(result);
+    display_results(result, numTests);
     free_all(result[0], result[1]);
     exit(EXIT_SUCCESS);
 }
 
-int gather_results(int *result[])
+int gather_results(int *result[])/*#{{{*/
 {
     int fd       = 0;   /* file descriptor */
     int numTests = 0;   /* number of tests in file */
@@ -57,7 +62,7 @@ int gather_results(int *result[])
     i = j = minOne = minTwo = prev = cur = 0;
     
     /* open the sample file */
-    fd = open("smallpractice", O_RDWR);
+    fd = open("largePractice", O_RDWR);
     if(fd == -1){
         errExit("gather_results: Opening file failed");}
     
@@ -88,10 +93,13 @@ int gather_results(int *result[])
         --sampNum; /* prevVal holds the first sample */
         for(j = 0; j < sampNum; ++j)
         {
+            /* get next value in the buffer */
             getNumString(inBuff, bufPl, numStr);
             cur = getInt(numStr, 0, "cur");
             
+            /* gather the minimums */
             minOne += method_one(prev, cur);
+            minTwo += method_two(sampNum+1, prev,  cur);
 
             prev = cur;
         }
@@ -102,22 +110,19 @@ int gather_results(int *result[])
             minOne = prev;
             minTwo = prev;
         }
-
-        printf("\nminOne: %d\n", minOne);
+        
+        /* fill result */
         result[0][i] = minOne;
         result[1][i] = minTwo;
-
-        //printf("\ncase: %d", minOne);
     }
 
-    
     if(close(fd) == -1){
         errExit("gather_results: close() failure");}
 
-    return 0;
-} /* end gather_results */
+    return numTests;
+} /* end gather_results #}}} */
 
-int method_one(int prev, int cur)
+int method_one(int prev, int cur)/*#{{{*/
 {
     int res = 0;
 
@@ -127,4 +132,35 @@ int method_one(int prev, int cur)
         return res;
     }
     return 0;
-} /* end method_one */
+} /* end method_one #}}} */
+
+int method_two(int sampNum, int prev, int cur)/*#{{{*/
+{
+#define MEPI 10 /* min eaten per interval */
+    int min = (sampNum * MEPI);
+
+    if(prev < cur){
+        min = prev;}
+    else{
+        min = 10;}
+    return min;
+#undef MEPI
+} /* end method_two #}}} */
+
+void display_results(int *result[], int numTests)/*#{{{*/
+{
+#define TM_BF_  4096
+    int i = 0;
+    char temp[TM_BF_] = {'\0'};
+    
+    if(result == NULL || *result == NULL){
+        errExit("display_results(): *result[] was NULL");}
+   
+    for(i = 0; i < numTests; ++i)
+    {
+        snprintf(temp, TM_BF_,"\nCase %d: %d %d", 
+                 i+1, result[0][i], result[1][i]);
+        write(STDOUT_FILENO, temp, strlen(temp));
+    }
+#undef TM_BF_
+} /* end display_results #}}} */
--- sedih/mushMonster/mush.h
+++ sedih/mushMonster/mush.h
@@ -5,12 +5,18 @@
 #include "../sedhead/sedhead.h"
 
 /* executes method one and returns the results in an int corresponding
-   to the result that will be printed. returns the samples min */
+   to the result that will be printed. returns the samples min.*/
 int method_one(int prev, int cur);
+
+/* executes method two and returns the results in an int corresponding
+   to the result that will be printed. returns the samples min */
+int method_two();
+
 /* goes through the file, pulls all the stuff, calls the methods for the stuff.
    that stuff.
    results[0] = method1;
    results[1] = method2; */
 int gather_results(int *result[]);
+
 /* print the results of each method */
 void display_results(int *result[], int numTests);

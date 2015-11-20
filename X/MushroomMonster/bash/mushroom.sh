d9864de207f3442dcd83ee5838e9d7e9fcc69151
Yu Shyuan Tang <ytang@ada.cs.pdx.edu>
2015-11-20 03:53:29 -0800

    completed MushroomMonster in bash

--- X/MushroomMonster/bash/mushroom.sh
+++ X/MushroomMonster/bash/mushroom.sh
@@ -3,27 +3,41 @@
 read -r numberOfCases 
 nums=$(($numberOfCases))
 
-for i in `seq 1 $nums`
+for j in `seq 1 $nums`
   do
     read -r numPlates
-    #echo NUM PLATES
-    #echo $numPlates 
-    #echo ----------------
-    total=0
+    total1=0
+    total2=0
+    maxDiff=0
+    
 
     read -r line
     plates=($line)
     for ((i=1; i < $((${#plates[@]} )); i++))
     do
-      diff=$((${plates[$(($i - 1))]}-${plates[$i]}))
-      if [ $diff -ge 0 ]; then
-        total=$(($total + $diff))
+      diff=$((  ${plates[$(($i - 1))]}  -  ${plates[$i]} ))
+      if [ $diff -ge 0 ]
+      then
+        total1=$(($total1 + $diff))
       fi
-      #total=$(($total + plates[$i]
-      #$echo $total
+      if [ $diff -ge $maxDiff ]
+      then
+        maxDiff=$diff
+      fi
     done
-   echo $total
-   total=0 
+    
+    for ((i=0; i < $(( ${#plates[@]} - 1 )); i++))
+    do
+      if [ $maxDiff -le ${plates[$i]} ]
+      then
+        total2=$(( $total2 + $maxDiff ))
+        
+      else
+        total2=$(( $total2 + ${plates[$i]} ))
+      fi
+    done
+
+   echo Case \#$j\: $total1 $total2
 
   done
 
